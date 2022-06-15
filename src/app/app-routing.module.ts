4dbdc7b2ba6a00282a21d5b3ed99fdbb272f3167
homesoftcore <homesoftcore@gmail.com>
2022-06-14 21:38:17 -0500

    end list user ngtyle ngclass

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NgStyleComponent } from './components/directives/ng-style/ng-style.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'ng-style', component: NgStyleComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,27 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { AsideComponent } from './components/aside/aside.component';
 import { MainComponent } from './components/main/main.component';
+import { NgClassComponent } from './components/directives/ng-class/ng-class.component';
+import { NgStyleComponent } from './components/directives/ng-style/ng-style.component';
+import { ChildComponent } from './components/interpolation/child/child.component';
+import { ParentComponent } from './components/interpolation/parent/parent.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     AsideComponent,
-    MainComponent
+    MainComponent,
+    NgClassComponent,
+    NgStyleComponent,
+    ChildComponent,
+    ParentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

258929bfd5502babe8caa313e5b67ed08b0a2f97
homesoftcore <homesoftcore@gmail.com>
2022-06-17 10:32:54 -0500

    add changes

---
+++ src/app/components/directives/ng-class/ng-class.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-class',
+  templateUrl: './ng-class.component.html',
+  styleUrls: ['./ng-class.component.css']
+})
+export class NgClassComponent implements OnInit {
+
+  public error:boolean=true;
+  public nombre:string='David';
+  public apellido:string='Casa';
+  public descripcion:string='Soy DevOps';
+  public ListUsers=[
+    {"nombre":"David","apellido":"Casa","edad":18, "ft":12},
+    {"nombre":"Pedro","apellido":"Castellano","edad":15, "ft":10},
+    {"nombre":"Luis","apellido":"Ruiz","edad":21, "ft":9},
+    {"nombre":"Javier","apellido":"Endara","edad":65, "ft":15},
+  ]
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
